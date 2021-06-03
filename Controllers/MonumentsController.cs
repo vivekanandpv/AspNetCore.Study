c179ba07dbe2e6d979653c4135ac4723f986de40
vivek <vivek@athenaeum.in>
2021-06-03 12:41:05 +0530

    Add project files.

---
+++ Controllers/MonumentsController.cs
@@ -0,0 +1,46 @@
+﻿using AspNetCore.Study.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCore.Study.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MonumentsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Welcome");
+        }
+
+
+        [HttpGet("{id:int}")]
+        public IActionResult Get(int id)
+        {
+            return Ok($"Returning id: {id}");
+        }
+
+        [HttpPost]
+        public IActionResult Create(Monument monument)
+        {
+            return Ok();
+        }
+
+        [HttpPut]
+        public IActionResult Update(Monument monument)
+        {
+            return Ok();
+        }
+
+        [HttpDelete("{id:int}")]
+        public IActionResult Delete(int id)
+        {
+            return Ok($"Deleting id: {id}");
+        }
+    }
+}
---
+++ Models/Monument.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCore.Study.Models
+{
+    public class Monument
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string City { get; set; }
+        public double EntryFee { get; set; }
+        public int StartHour { get; set; }
+        public int EndHour { get; set; }
+    }
+}
